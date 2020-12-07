92811097c44574a7241241244168de397d6a205f
MillaMay <38451109+MillaMay@users.noreply.github.com>
2020-12-07 11:55:10 +0200

    Delete currency-value.php

--- config/currency-value.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'api_url' => 'https://api.exchangeratesapi.io/latest',
-    /* в конце этой строки если подставить ?base=USD, то можно изменять валюту,
-    например, ?base=RUB */
-];
